e177f71ebe5aac0f161ece717dce34dce269c9a4
theehmedov <the.ehmedov008@gmail.com>
2026-01-09 05:54:10 -0500

    nmap

--- network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
+++ network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-
-# Arqumentin (alt şəbəkənin) daxil edildiyini yoxlayır
-if [ -z "$1" ]; then
-  echo "İstifadə qaydası: $0 <alt_şəbəkə>"
-  exit 1
-fi
-
-# nmap-i sudo (root) hüquqları ilə işə salır
-# -sn : Port skanını söndürür (yalnız Host Discovery). 
-#       Qeyd: Yerli şəbəkədə root hüquqları ilə işlədikdə, bu avtomatik olaraq ARP skanı edir.
 sudo nmap -sn $1
