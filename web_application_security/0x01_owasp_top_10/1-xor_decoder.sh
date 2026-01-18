db191234e8b99c5b515901d70a843f5af1377e09
theehmedov <the.ehmedov008@gmail.com>
2026-01-18 13:35:46 -0500

    nmap

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,1 +1,6 @@
-b4c4e18917b60d32838237dc431e997f
+#!/bin/bash
+if [ -z "$1" ]; then
+    exit 1
+fi
+encoded_string=${1#"{xor}"}
+python3 -c "import base64; print(''.join([chr(b ^ 95) for b in base64.b64decode('$encoded_string')]))"
