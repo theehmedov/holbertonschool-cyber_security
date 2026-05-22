5a03aa6ff697fb4c098e21f943d76a5464725fa9
theehmedov <the.ehmedov008@gmail.com>
2026-05-22 08:35:46 -0400

    cyber test

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -2,7 +2,9 @@ def print_arguments
   if ARGV.empty?
     puts "No arguments provided."
   else
-    args_str = ARGV.map.with_index { |arg, i| "#{i + 1}. #{arg}" }.join(", ")
-    puts "Arguments: #{args_str}"
+    puts "Arguments:"
+    ARGV.each_with_index do |arg, index|
+      puts "   #{index + 1}. #{arg}"
+    end
   end
 end
