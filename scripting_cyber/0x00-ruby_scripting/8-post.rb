fd19302c8fbfcbbe0a7918158bba4eb31ffd3d8c
theehmedov <the.ehmedov008@gmail.com>
2026-05-22 08:36:43 -0400

    cyber test

---
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -0,0 +1,20 @@
+require 'net/http'
+require 'json'
+
+def post_request(url, body_params)
+  uri = URI(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true if uri.scheme == 'https'
+  
+  request = Net::HTTP::Post.new(uri.path)
+  request['Content-Type'] = 'application/json'
+  request.body = JSON.generate(body_params)
+  
+  response = http.request(request)
+  
+  puts "Response status: #{response.code} #{response.message}"
+  puts "Response body:"
+  
+  body = JSON.parse(response.body)
+  puts JSON.pretty_generate(body)
+end
