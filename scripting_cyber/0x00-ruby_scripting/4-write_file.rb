36b2c63550e503b75213a7e19754b6aeaae6c943
theehmedov <the.ehmedov008@gmail.com>
2026-05-22 08:26:11 -0400

    cyber test

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -10,4 +10,6 @@ def merge_json_files(file1_path, file2_path)
   merged_data = data2 + data1
   
   File.write(file2_path, JSON.pretty_generate(merged_data))
+  
+  puts "Merged JSON written to #{file2_path}"
 end
